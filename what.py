cf2a82c3b5c16a77a0f00c4b5a30d6e11afcd220
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-01-30 22:59:01 +0530

    first commit

---
+++ what.py
@@ -0,0 +1,8 @@
+import pywhatkit
+# PyWhatKit is a Python library for Sending whatsapp message at certain time
+
+#This will open web.whatsapp.com at 22:56:40 
+#and message will be sent at exactly 22:57:00
+
+pywhatkit.sendwhatmsg("+917297016688","This is a message",22,57)
+
